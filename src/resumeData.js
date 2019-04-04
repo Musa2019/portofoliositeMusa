f77bfeb5830cf7fd8c0503956ffdcf06c531b589
Musa2019 <47635303+Musa2019@users.noreply.github.com>
2019-04-04 16:16:19 +0200

    .gitignore

---
+++ src/resumeData.js
@@ -0,0 +1,100 @@
+let resumeData = {
+    "imagebaseurl": "https://github.com/Musa2019/.git/",
+    "name": "Musa Hakizimana",
+    "role": "Full-stack Developper",
+    "linkedinId": "https://www.linkedin.com/in/musa-hakizimana",
+    "skypeid": "MusaHakizimana",
+    "roleDescription": "I am a full-stack developper, i like programming and read books in my free time",
+    "socialLinks": [{
+            "musa-hakizimana": "linkedin",
+            "url": "https://www.linkedin.com/",
+            "className": "musa-hakizimana-linkedin"
+        },
+        {
+            "Musa2019": "github",
+            "url": "http://github.com/",
+            "className": "Musa2019-github"
+        },
+        {
+            "MusaHakizimana": "skype",
+            "url": "http://skype.com",
+            "className": "MusaHakizimana-skype"
+        }
+    ],
+    "aboutme": "I have finished my Bacherlor degree in Economics and Business Studies at Kigali Independent University.",
+    "address": "Rwanda",
+    "website": "",
+    "education": [{
+            "UniversityName": "Kigali Indepedent University",
+            "specialization": "Web-Development, Data Analysis",
+            "MonthOfPassing": "Dec",
+            "YearOfPassing": "2016",
+            "Achievements": "Bacherlor Degree"
+        },
+        {
+            "UniversityName": "Some University",
+            "specialization": "Some specialization",
+            "MonthOfPassing": "",
+            "YearOfPassing": "",
+            "Achievements": "Some Achievements"
+        }
+    ],
+    "work": [{
+            "CompanyName": "Delagua Health Rwanda",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "",
+            "YearOfLeaving": "",
+            "Achievements": "Awarded Twice"
+        },
+        {
+            "CompanyName": "Some Company",
+            "specialization": "Some specialization",
+            "MonthOfLeaving": "",
+            "YearOfLeaving": "",
+            "Achievements": "Some Achievements"
+        }
+    ],
+    "skillsDescription": "Full Stack Developper",
+    "skills": [{
+            "Excellent": "HTML5"
+        },
+        {
+            "Excellent": "CSS"
+        },
+        {
+            "Excellent": "Reactjs"
+        }
+    ],
+    "portfolio": [{
+            "Musa Hakizimana": "Site Portofolio",
+            "description": "Single Page React",
+            "imgurl": "images/portfolio/phone.jpg"
+        },
+        {
+            "name": "project2",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project.jpg"
+        },
+        {
+            "name": "project3",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/project2.png"
+        },
+        {
+            "name": "project4",
+            "description": "mobileapp",
+            "imgurl": "images/portfolio/phone.jpg"
+        }
+    ],
+    "testimonials": [{
+            "description": "This is a sample testimonial",
+            "name": "Andelan"
+        },
+        {
+            "description": "This is a sample testimonial",
+            "name": "Some technical guy"
+        }
+    ]
+}
+
+export default resumeData
